dad11e37707116aa156fef2722007dc48bf1d06f
guilhermeoliveira17 <guilhermeoliveira13579112@gmail.com>
2023-11-26 18:28:16 -0300

    delete ator completo

--- BazingaFilmes/Paginas/Atores/DeletarAtor.aspx.cs
+++ BazingaFilmes/Paginas/Atores/DeletarAtor.aspx.cs
@@ -20,5 +20,32 @@ namespace BazingaFilmes.Paginas.Atores
                 lblAtor.InnerText = result[0].nome;
             }
         }
+
+        protected void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Paginas/Idioma/Idioma.aspx");
+        }
+
+        protected void btnApagar_Click(object sender, EventArgs e)
+        {
+            //Realiza a exclusão física no banco e verifica os possíveis erros retornados
+            int? retorno = null;
+            BazingaFilmesDSTableAdapters.AtorTableAdapter dt = new BazingaFilmesDSTableAdapters.AtorTableAdapter();
+            dt.DeleteAtor(id, ref retorno);
+            if (retorno == -1)
+            {
+                lblMensagemErro.InnerText = "Erro: não é possível excluir um ator vinculado a um filme!";
+                alert.Visible = true;
+            }
+            else if (retorno == 1)
+            {
+                lblMensagemErro.InnerText = "Erro: entre em contato com o administrador!";
+                alert.Visible = true;
+            }
+            else if (retorno == 0)
+            {
+                Response.Redirect("~/Paginas/Atores/Ator.aspx");
+            }
+        }
     }
 }
