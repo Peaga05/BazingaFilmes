debf16574cc464954a1339b8c36f52aa952f6f72
guilhermeoliveira17 <guilhermeoliveira13579112@gmail.com>
2023-11-26 13:23:06 -0300

    Teste Cadastro de ator

--- BazingaFilmes/Paginas/Atores/CadastrarAtor.aspx.cs
+++ BazingaFilmes/Paginas/Atores/CadastrarAtor.aspx.cs
@@ -13,5 +13,38 @@ namespace BazingaFilmes.Paginas.Atores
         {
 
         }
+
+        protected void btnSalvar_Click(object sender, EventArgs e)
+        {
+            //Realiza o cadastro no banco e exibe mensagens de erro
+            string nome = txtNome.Text;
+            string sobrenome = txtSobrenome.Text;
+            int? ret = null;
+            if (nome == null || nome == "" || sobrenome == null || sobrenome == "")
+            {
+                alert.Visible = true;
+                lblMensagemErro.InnerText = "Campo vazio!";
+            }
+            else
+            {
+                BazingaFilmesDSTableAdapters.AtorTableAdapter dt = new BazingaFilmesDSTableAdapters.AtorTableAdapter();
+                dt.InsertAtor(nome, sobrenome, ref ret);
+                if (ret == -1)
+                {
+                    alert.Visible = true;
+                    lblMensagemErro.InnerText = "Erro: Esse ator já está cadastrado!";
+                }
+                else
+                {
+                    Response.Redirect("~/Paginas/Atores/Ator.aspx");
+                }
+            }
+
+        }
+
+        protected void btnVoltar_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Paginas/Atores/Ator.aspx");
+        }
     }
 }
